4c2d58a982823ce3c5dd70b5901817cc6589f21a
Farhad Poorsolaymani <farhad4051@gmail.com>
2026-06-11 13:21:07 +0200

    Assembly

---
+++ Assets/Game/Scripts/Components/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Game.Runtime")]
+[assembly: InternalsVisibleTo("Game.Editor")]
+[assembly: InternalsVisibleTo("Game.Tests.Editor")]
+[assembly: InternalsVisibleTo("Game.Tests.PlayMode")]

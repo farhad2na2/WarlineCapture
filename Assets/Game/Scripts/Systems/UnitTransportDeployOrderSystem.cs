06fcd923270e334eb51d559cccb54a55a25b76d4
Farhad Poorsolaymani <farhad4051@gmail.com>
2026-06-23 19:06:42 +0200

    Architecture & Performance Remediation Progress Tracker

--- Assets/Game/Scripts/Systems/UnitTransportDeployOrderSystem.cs
+++ Assets/Game/Scripts/Systems/UnitTransportDeployOrderSystem.cs
@@ -1,7 +1,6 @@
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Transforms;
 
 [UpdateAfter(typeof(UnitAirMovementSystem))]
 [UpdateAfter(typeof(UnitGridMovementSystem))]

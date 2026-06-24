4a0478c0cb728e4c3014318a8f30ea6277d3c83d
Farhad Poorsolaymani <farhad4051@gmail.com>
2026-06-24 21:56:12 +0200

    Fix ECS startup ordering cycle

--- Assets/Game/Scripts/Systems/UnitIdleWanderSystem.cs
+++ Assets/Game/Scripts/Systems/UnitIdleWanderSystem.cs
@@ -4,7 +4,6 @@ using Unity.Entities;
 using Unity.Mathematics;
 
 [BurstCompile]
-[UpdateAfter(typeof(UnitMoveVisualStateSystem))]
 [UpdateBefore(typeof(UnitPathfindingSystem))]
 public partial struct UnitIdleWanderSystem : ISystem
 {
